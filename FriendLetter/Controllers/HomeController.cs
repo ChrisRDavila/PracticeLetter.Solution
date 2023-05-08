219572cee96f8078432858e6582376f8e3597ca7
ChrisRDavila <crossdavila@gmail.com>
2023-05-08 10:38:11 -0700

    add controller

---
+++ FriendLetter/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/")]
+    // Notice the changes below!
+    public ActionResult Letter() { return View(); }
+
+  }
+}
